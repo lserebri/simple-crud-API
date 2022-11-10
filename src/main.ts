38843f46892f9d1d761b122685ed04d9f4c3bfcc
lserebriakovDigis <l.serebriakov@connectbase.com>
2022-11-10 00:51:10 +0000

    Changed log in main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
   await app.listen(port, () => {
-    console.log(`Server is listening on port ${port}}`);
+    console.log(`Server is listening on port ${port}`);
   });
 }
 bootstrap();
